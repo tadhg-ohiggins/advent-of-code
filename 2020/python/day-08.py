875c010c4aea314be7fee67ea15d79e43be3dff3
Tadhg O'Higgins <tadhg@tadhg.com>
2020-12-08 14:23:55 -0800

    After day eight.

--- 2020/python/day-08.py
+++ 2020/python/day-08.py
@@ -1,171 +1,11 @@
-import pdb
-import subprocess
-from functools import partial, reduce, wraps
-from math import prod
+from functools import partial
 from pathlib import Path
-from pprint import pprint
-from string import (
-    ascii_lowercase,
-    digits as ascii_digits,
-)
-from typing import Any, Callable, List, Iterable, Optional, Union
-from toolz import (  # type: ignore
-    compose_left,
-    concat,
-    curry,
-    do,
-    excepts,
-    iterate,
-    keyfilter,
-    pluck,
-    pipe,
-)
-
-
-IterableS = Iterable[str]
-hexc = ["a", "b", "c", "d", "e", "f"] + list(ascii_digits)
-
-
-def toolz_pick(keep: IterableS, d: dict) -> dict:
-    return keyfilter(lambda x: x in keep, d)
-
-
-def toolz_omit(remove: IterableS, d: dict) -> dict:
-    return keyfilter(lambda x: x not in remove, d)
-
-
-def pick(keep: IterableS, d: dict) -> dict:
-    return {k: d[k] for k in d if k in keep}
-
-
-def omit(remove: IterableS, d: dict) -> dict:
-    return {k: d[k] for k in d if k not in remove}
-
-
-def add_debug(debug_f: Callable, orig_f: Callable) -> Callable:
-    """
-    Transforms the function such that output is passed
-    to the debug function before being returned as normal.
-
-    add_debug(print, str.upper) would return a function equivalent to:
-
-    def fn(val: str): -> str
-        result = str.upper(val)
-        print(result)
-        return result
-    """
-    do_f = partial(do, debug_f)
-    return compose_left(orig_f, do_f)
-
-
-def add_debug_list(debug_f: Callable, funcs: List[Callable]) -> List[Callable]:
-    """
-    Transforms each of the functions such that the output of each is passed
-    to the debug function before being returned as normal.
-    """
-    return [add_debug(debug_f, f) for f in funcs]
-
-
-def run_process(
-    command: Union[list, str], options: Optional[dict] = None
-) -> subprocess.CompletedProcess:
-    base_opts = {"check": True, "text": True, "capture_output": True}
-    opts = options if options else {}
-    # pylint: disable=subprocess-run-check
-    # return subprocess.run(command, **{**base_opts, **opts})  # type: ignore
-    return subprocess.run(command, **(base_opts | opts))  # type: ignore
-
-
-def until_stable(func: Callable) -> Callable:
-    """
-    Repeatedly call the same function on its arguments until the result doesn't
-    change.
-
-    Not sure how to make this work in variadic cases; comparing a single result
-    to *args doesn't seem to work.
-    """
-
-    def inner(arg: Any, **kwds: Any) -> Any:
-        if func(arg, **kwds) == arg:
-            return arg
-        return inner(func(arg, **kwds))
-
-    return inner
-
-
-def until_stable(func: Callable) -> Callable:
-    """
-    Repeatedly call the same function on its arguments until the result doesn't
-    change.
-
-    Not sure how to make this work in variadic cases; comparing a single result
-    to *args doesn't seem to work.
-    """
-
-    def inner(arg: Any, **kwds: Any) -> Any:
-        if func(arg, **kwds) == arg:
-            return arg
-        return inner(func(arg, **kwds))
-
-    return inner
-
-
-def oxford(lst: List[str]) -> str:
-    """
-    Turns a list into a properly-formatted list phrase.
-    ``["something"]`` becomes "something".
-    ``["thing1", "thing2"]`` becomes "thing1 and thing2".
-    ``["thing1", "thing2", "thing3"]`` becomes "thing1, thing2, and thing3".
-    ``["a", "b", "c", "d"]`` becomes "a, b, c, and d".
-    """
-    if len(lst) <= 2:
-        return " and ".join(lst)
-    return f'{", ".join(lst[:-1])}, and {lst[-1]}'
-
-
-def excepts_wrap(err: Any, err_func: Callable) -> Callable:
-    """
-    This basically means that::
-
-        @excepts_wrap(ValueError, lambda _: None)
-        def get_formatted_time(fmt: str, value: str) -> Optional[datetime]:
-            return datetime.strptime(value.strip(), fmt)
-
-        gft = get_formatted_time
-
-    With the decorator, that's broadly equivalent to this without
-    any decorator::
-
-        gft = excepts(
-            ValueError,
-            get_formatted_time,
-            lambda _: None
-        )
-
-    """
-
-    def inner_excepts_wrap(fn: Callable) -> Callable:
-        return excepts(err, fn, err_func)
-
-    return inner_excepts_wrap
+from toolz import compose_left, iterate  # type: ignore
 
 
 lfilter = compose_left(filter, list)  # lambda f, l: [*filter(f, l)]
-lmap = compose_left(map, list)  # lambda f, l: [*map(f, l)]
-lpluck = compose_left(pluck, list)  # lambda k, l: [*pluck(f, l)]
-c_map = curry(map)
-c_lmap = curry(lmap)
-is_char_az = partial(lambda y, x: x in y, ascii_lowercase)
-is_char_hex = partial(lambda y, x: x in y, hexc)
-is_char_az09 = partial(lambda y, x: x in y, ascii_lowercase + ascii_digits)
-filter_str = partial(lambda f, s: "".join(filter(f, s)))
-filter_az = partial(filter_str, is_char_az)
-filter_az09 = partial(filter_str, is_char_az09)
-filter_hex = partial(filter_str, is_char_hex)
-add_pprint = partial(add_debug, pprint)
-add_pprinting = partial(lmap, add_pprint)
 lcompact = partial(lfilter, None)
-splitstrip = compose_left(str.split, partial(lmap, str.strip), lcompact)
+splitstrip = compose_left(str.split, partial(map, str.strip), lcompact)
 
 
 def proc_line(lines, current, acc, seen):
@@ -248,3 +88,132 @@ if __name__ == "__main__":
     raw = Path("input-08.txt").read_text()
     raw = raw.strip()  # comment this out if trailing stuff is important!
     result = process(raw)
+
+"""
+--- Day 8: Handheld Halting ---
+
+Your flight to the major airline hub reaches cruising altitude without
+incident. While you consider checking the in-flight menu for one of those
+drinks that come with a little umbrella, you are interrupted by the kid sitting
+next to you.
+
+Their handheld game console won't turn on! They ask if you can take a look.
+
+You narrow the problem down to a strange infinite loop in the boot code (your
+puzzle input) of the device. You should be able to fix it, but first you need
+to be able to run the code in isolation.
+
+The boot code is represented as a text file with one instruction per line of
+text. Each instruction consists of an operation (acc, jmp, or nop) and an
+argument (a signed number like +4 or -20).
+
+    acc increases or decreases a single global value called the accumulator by
+    the value given in the argument. For example, acc +7 would increase the
+    accumulator by 7. The accumulator starts at 0. After an acc instruction,
+    the instruction immediately below it is executed next.
+
+    jmp jumps to a new instruction relative to itself. The next instruction to
+    execute is found using the argument as an offset from the jmp instruction;
+    for example, jmp +2 would skip the next instruction, jmp +1 would continue
+    to the instruction immediately below it, and jmp -20 would cause the
+    instruction 20 lines above to be executed next.
+
+    nop stands for No OPeration - it does nothing. The instruction immediately
+    below it is executed next.
+
+For example, consider the following program:
+
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+
+These instructions are visited in this order:
+
+nop +0  | 1
+acc +1  | 2, 8(!)
+jmp +4  | 3
+acc +3  | 6
+jmp -3  | 7
+acc -99 |
+acc +1  | 4
+jmp -4  | 5
+acc +6  |
+
+First, the nop +0 does nothing. Then, the accumulator is increased from 0 to 1
+(acc +1) and jmp +4 sets the next instruction to the other acc +1 near the
+bottom. After it increases the accumulator from 1 to 2, jmp -4 executes,
+setting the next instruction to the only acc +3. It sets the accumulator to 5,
+and jmp -3 causes the program to continue back at the first acc +1.
+
+This is an infinite loop: with this sequence of jumps, the program will run
+forever. The moment the program tries to run any instruction a second time, you
+know it will never terminate.
+
+Immediately before the program would run an instruction a second time, the
+value in the accumulator is 5.
+
+Run your copy of the boot code. Immediately before any instruction is executed
+a second time, what value is in the accumulator?
+
+Your puzzle answer was 1675.
+
+--- Part Two ---
+
+After some careful analysis, you believe that exactly one instruction is
+corrupted.
+
+Somewhere in the program, either a jmp is supposed to be a nop, or a nop is
+supposed to be a jmp. (No acc instructions were harmed in the corruption of
+this boot code.)
+
+The program is supposed to terminate by attempting to execute an instruction
+immediately after the last instruction in the file. By changing exactly one jmp
+or nop, you can repair the boot code and make it terminate correctly.
+
+For example, consider the same program from above:
+
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+
+If you change the first instruction from nop +0 to jmp +0, it would create a
+single-instruction infinite loop, never leaving that instruction. If you change
+almost any of the jmp instructions, the program will still eventually find
+another jmp instruction and loop forever.
+
+However, if you change the second-to-last instruction (from jmp -4 to nop -4),
+the program terminates! The instructions are visited in this order:
+
+nop +0  | 1
+acc +1  | 2
+jmp +4  | 3
+acc +3  |
+jmp -3  |
+acc -99 |
+acc +1  | 4
+nop -4  | 5
+acc +6  | 6
+
+After the last instruction (acc +6), the program terminates by attempting to
+run the instruction below the last instruction in the file. With this change,
+after the program terminates, the accumulator contains the value 8 (acc +1, acc
++1, acc +6).
+
+Fix the program so that it terminates normally by changing exactly one jmp (to
+nop) or nop (to jmp). What is the value of the accumulator after the program
+terminates?
+
+Your puzzle answer was 1532.
+"""
