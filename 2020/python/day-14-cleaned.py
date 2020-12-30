c9fac5bb449240d3ed2ae739e96d05c3f061ab28
Tadhg O'Higgins <tadhg@tadhg.com>
2020-12-30 00:35:08 -0800

    Add refactored version of code for day 14.

--- 2020/python/day-14-cleaned.py
+++ 2020/python/day-14-cleaned.py
@@ -1,51 +1,68 @@
-from collections import Counter, defaultdict
 from functools import partial, reduce
 from itertools import product
-from pathlib import Path
+from typing import Callable, Iterable, List, Tuple
 from more_itertools import split_before
-from toolz import compose_left, iterate  # type: ignore
+
+from tutils import (
+    lmap,
+    splitstrip,
+    splitstriplines,
+    load_and_process_input,
+    run_tests,
+)
+
+DAY = "14"
+INPUT, TEST = f"input-{DAY}.txt", f"test-input-{DAY}.txt"
+TA1 = None
+TA2 = None
+ANSWER1 = 12408060320841
+ANSWER2 = 4466434626828
 
 
-lfilter = compose_left(filter, list)  # lambda f, l: [*filter(f, l)]
-lmap = compose_left(map, list)  # lambda f, l: [*map(f, l)]
-lcompact = partial(lfilter, None)
-splitstrip = compose_left(str.split, partial(lmap, str.strip), lcompact)
-make_incr = lambda: partial(next, iterate(lambda x: x + 1, 0))
-sum_values = lambda d: sum(d.values())
-int_to_36bit = lambda i: bin(i).removeprefix("0b").zfill(36)
+def int_to_36bit(i: int) -> str:
+    return bin(i).removeprefix("0b").zfill(36)
 
 
-def parsecommand(line):
+def parsecommand(line: str) -> dict:
     regstr, value = splitstrip(line, " = ")
     register = int(regstr.removeprefix("mem[").removesuffix("]"))
     return {"r": int(register), "v": int(value)}
 
 
-def parse_lines(lines):
-    mask = splitstrip(lines[0], " = ")[1]
-    commands = lmap(parsecommand, lines[1:])
+def parse_group(group: List[str]) -> Tuple[str, List[dict]]:
+    mask = splitstrip(group[0], " = ")[1]
+    commands = lmap(parsecommand, group[1:])
     return mask, commands
 
 
-def apply_mask(mask, registers, command):
+def lines_to_programs(lines: List[str]) -> List[Tuple[str, List[dict]]]:
+    """
+    Programs will be represented as two-part tuples. The first part is the
+    mask, a string. The second part is a list of registers and values,
+    represented by a dict with the keys "r" and "v".
+    """
+    return lmap(parse_group, split_before(lines, lambda s: s[:4] == "mask"))
+
+
+def apply_mask(maskstr: str, registers: dict, command: dict) -> dict:
     register, value = command["r"], int_to_36bit(command["v"])
-    masked = [mask.get(i, x) for i, x in enumerate(value)]
-    return registers | {register: int("".join(masked), 2)}
-
-
-def process_program(registers, lines):
-    maskstr, commands = parse_lines(lines)
     mask = {i: bit for i, bit in enumerate(maskstr) if bit in ("0", "1")}
-    masker = partial(apply_mask, mask)
+    masked = [mask.get(i, x) for i, x in enumerate(value)]
+    return {**registers, **{register: int("".join(masked), 2)}}
+
+
+def process_program(func: Callable, registers: dict, program: Tuple) -> dict:
+    mask, commands = program
+    masker = partial(func, mask)
     return reduce(masker, commands, registers)
 
 
-def generate_combination_addresses(base_address):
+def generate_combination_addresses(base_address: List[str]) -> Iterable[str]:
     # For every X in the string, we need all combinations of 1 and 0, so
     # count(X) ** 2 of them.
     xcount = base_address.count("X")
     if not xcount:
-        return [base_address]
+        return ["".join(base_address)]
     combinations = product(*[[0, 1]] * xcount)
     # For each X, replace it with the correspondiing bit from the combo:
     replace_x = lambda x, y: x.replace("X", str(y), 1)
@@ -53,7 +70,7 @@ def generate_combination_addresses(base_address):
     return map(replace_all_xs, combinations)
 
 
-def apply_mask2(mask, registers, command):
+def apply_mask2(mask: str, registers: dict, command: dict) -> dict:
     address, value = list(int_to_36bit(command["r"])), command["v"]
     masked = [b if b in ("1", "X") else address[i] for i, b in enumerate(mask)]
 
@@ -62,23 +79,27 @@ def apply_mask2(mask, registers, command):
     return registers
 
 
-def process_program2(registers, lines):
-    mask, commands = parse_lines(lines)
-    masker = partial(apply_mask2, mask)
-    return reduce(masker, commands, registers)
+def process_one(programs: List[Tuple]) -> int:
+    process_program1 = partial(process_program, apply_mask)
+    return sum(reduce(process_program1, programs, {}).values())
 
 
-def process(text):
-    lines = lcompact(text.splitlines())
-    programs = list(split_before(lines, lambda s: s.startswith("mask")))
-    answer = sum_values(reduce(process_program, programs, {}))
-    assert answer == 12408060320841
+def process_two(programs: List[Tuple]) -> int:
+    process_program2 = partial(process_program, apply_mask2)
+    return sum(reduce(process_program2, programs, {}).values())
 
-    answer_two = sum_values(reduce(process_program2, programs, {}))
-    assert answer_two == 4466434626828
-    print("Answer one:", answer)
+
+def cli_main() -> None:
+    input_funcs = [splitstriplines, lines_to_programs]
+    data = load_and_process_input(INPUT, input_funcs)
+    run_tests(TEST, TA1, TA2, ANSWER1, input_funcs, process_one, process_two)
+    answer_one = process_one(data)
+    assert answer_one == ANSWER1
+    print("Answer one:", answer_one)
+    answer_two = process_two(data)
+    assert answer_two == ANSWER2
     print("Answer two:", answer_two)
 
 
 if __name__ == "__main__":
-    process(Path("input-14.txt").read_text().strip())
+    cli_main()
