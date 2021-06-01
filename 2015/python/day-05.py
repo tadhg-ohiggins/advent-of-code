980b71daed237cc8feaab49d4049e7b5a3b437f5
Tadhg O'Higgins <tadhg@tadhg.com>
2021-06-01 13:33:53 -0700

    2015 day 05 refactor.

--- 2015/python/day-05.py
+++ 2015/python/day-05.py
@@ -1,89 +1,97 @@
-from collections import Counter
-from pathlib import Path
-from typing import Iterable
-from toolz import compose_left, sliding_window  # type: ignore
+from tutils import Any
+from tutils import Counter
+from tutils import identity
+from tutils import lfilter
+from tutils import partitionby
+from tutils import pick
+from tutils import sliding_window
+from tutils import splitstriplines
+from tutils import valfilter
+
+from tutils import load_and_process_input
 
 
-IterableS = Iterable[str]
-lfilter = compose_left(filter, list)  # lambda f, l: [*filter(f, l)]
+""" END HELPER FUNCTIONS """
 
 
-def pick(keep: IterableS, d: dict) -> dict:
-    return {k: d[k] for k in d if k in keep}
+DAY = "05"
+INPUT = f"input-{DAY}.txt"
+ANSWER1 = 258
+ANSWER2 = 53
 
 
-def not_bad(text):
+def process_one(data: Any) -> Any:
+    return len(lfilter(is_nice, data))
+
+
+def is_nice(text: str) -> bool:
+    return all([no_bad_seqs(text), has_enough_vowels(text), consec(text)])
+
+
+def no_bad_seqs(text: str) -> bool:
     badseqs = ["ab", "cd", "pq", "xy"]
-    for seq in badseqs:
-        if seq in text:
-            return False
-    return True
+    return not any(seq in text for seq in badseqs)
 
 
-def has_vowels(text):
+def has_enough_vowels(text: str) -> bool:
     target = 3
-    vowels = list("aeiou")
-    ct = 0
-    counted = Counter(text)
-    for char in vowels:
-        ct = ct + counted.get(char, 0)
-    if ct >= target:
-        return True
-    return False
+    vowel_counts = pick("aeiou", Counter(text))
+    return sum(vowel_counts.values()) >= target
 
 
-def consec(text):
-    for i, char in enumerate(text):
-        if i + 1 != len(text):
-            if char == text[i + 1]:
-                return True
-    return False
+def consec(text: str) -> bool:
+    return any(len(_) > 1 for _ in partitionby(identity, text))
 
 
-def is_nice(text):
-    return all([not_bad(text), has_vowels(text), consec(text)])
+def process_two(data: str) -> int:
+    return len(lfilter(is_nice2, data))
 
 
-def twopair(text):
-    pairs = list(sliding_window(2, text))
-    spairs = [f"{_[0]}{_[1]}" for _ in pairs]
-    counted = Counter(spairs)
-    above_keys = [k for k in counted if counted[k] >= 2]
-    above = pick(above_keys, counted)
-    for k in above:
-        first = text.find(k)
-        if k in text[first + 2 :]:
-            return True
-    return False
-
-
-def repeater(text):
-    for triplet in sliding_window(3, text):
-        if triplet[0] == triplet[2]:
-            return True
-    return False
-
-
-def is_nice2(text):
+def is_nice2(text: str) -> bool:
     return all([twopair(text), repeater(text)])
 
 
-if __name__ == "__main__":
-    raw = Path("input-05.txt").read_text()
-    raw = raw.strip()
+def twopair(text: str) -> bool:
+    pairs = map("".join, sliding_window(2, text))
+    candidates = valfilter(lambda _: _ >= 2, Counter(pairs))
+    # Make sure we're not including e.g. aaa as aa and aa:
+    return any(k in text[text.find(k) + 2 :] for k in candidates)
+
+
+def repeater(text: str) -> bool:
+    return any(triplet[0] == triplet[2] for triplet in sliding_window(3, text))
+
+
+def tests_one():
     assert is_nice("ugknbfddgicrmopn")
     assert is_nice("aaa")
     assert not is_nice("jchzalrnumimnmhp")
     assert not is_nice("haegwjzuvuyypxyu")
     assert not is_nice("dvszwmarrgswjxmb")
-    nice = lfilter(is_nice, raw.splitlines())
-    print(len(nice))
+
+
+def tests_two():
     assert is_nice2("qjhvhtzxzqqjkmpb")
     assert is_nice2("xxyxx")
     assert not is_nice2("uurcxstgmygtbstg")
     assert not is_nice2("ieodomkazucvgmuy")
-    nice2 = lfilter(is_nice2, raw.splitlines())
-    print(len(nice2))
+
+
+def cli_main() -> None:
+    input_funcs = [splitstriplines]
+    data = load_and_process_input(INPUT, input_funcs)
+    tests_one()
+    answer_one = process_one(data)
+    assert answer_one == ANSWER1
+    print("Answer one:", answer_one)
+    tests_two()
+    answer_two = process_two(data)
+    assert answer_two == ANSWER2
+    print("Answer two:", answer_two)
+
+
+if __name__ == "__main__":
+    cli_main()
 
 
 """
