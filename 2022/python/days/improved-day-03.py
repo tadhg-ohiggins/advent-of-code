c1925ad09db47d763d83fdee417d0cf90917a6ab
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2022-12-03 11:31:11 -0800

    Day 03 improved going completely overboard refactor.

--- 2022/python/days/improved-day-03.py
+++ 2022/python/days/improved-day-03.py
@@ -1,39 +1,51 @@
+from operator import add
 from string import ascii_lowercase, ascii_uppercase
 from more_itertools import chunked, divide
-from toolz import pipe
-from tutils import c_lmap as cmap, lmap, splitstriplines
+from toolz import compose_left, curry
+from toolz.curried import flip
+from tutils import c_lmap as cmap, splitstriplines
 from tadhg_utils import star
-
 
 TEST_ANSWERS = (157, 70)
 PUZZLE_ANSWERS = (8053, 2425)
 
+chunk = compose_left(
+    flip(chunked)(3),  # Split into groups of 3.
+    list,
+)
 
-def chunk(items):
-    return list(chunked(items, 3))
+letterscore = compose_left(
+    (ascii_lowercase + ascii_uppercase).index,
+    curry(add)(1),
+)
 
+twosplit = compose_left(
+    curry(divide)(2),  # Divide in half.
+    cmap(list),
+)
 
-def letterscore(letter):
-    return 1 + (ascii_lowercase + ascii_uppercase).index(letter)
+get_common = compose_left(
+    cmap(set),
+    star(set.intersection),  # Pass all to intersection.
+    set.pop,
+)
 
+get_common_score_and_sum = compose_left(
+    cmap(get_common),
+    cmap(letterscore),
+    sum,
+)
 
-def twosplit(line):
-    return lmap(list, divide(2, line))
+# Functions expected by the puzzle runner below:
 
+preprocess = splitstriplines
 
-def get_common(group):
-    return pipe(group, *(cmap(set), star(set.intersection), set.pop))
+part_one = compose_left(
+    cmap(twosplit),
+    get_common_score_and_sum,
+)
 
-
-def preprocess(data):
-    return splitstriplines(data)
-
-
-def part_one(data):
-    procs = [cmap(twosplit), cmap(get_common), cmap(letterscore), sum]
-    return pipe(data, *procs)
-
-
-def part_two(data):
-    procs = [chunk, cmap(get_common), cmap(letterscore), sum]
-    return pipe(data, *procs)
+part_two = compose_left(
+    chunk,
+    get_common_score_and_sum,
+)
