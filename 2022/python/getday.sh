d935b089a4048eb9afc5509a87da3749ef53d10a
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2022-12-03 17:45:55 -0800

    Make setup script pre-type first command I'm likely to run in the other pane.

--- 2022/python/getday.sh
+++ 2022/python/getday.sh
@@ -12,4 +12,5 @@ tmux split-window -h -c "#{pane_current_path}";
 tmux select-pane -t .0;
 python fetch.py "${DAYNUM}";
 tmux send-keys -t .1 "cat data/input-""${DAYNUM}"".txt";
+tmux send-keys -t .1 "python day.py ${DAYNUM}";
 nvim -o2 "data/puzzle-""${DAYNUM}"".md" "days/day-""${DAYNUM}"".py" tutil.py
