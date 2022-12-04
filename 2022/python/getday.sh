f79cefaf75a37785acf31fce7e42edd94cda66d9
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2022-12-03 17:57:29 -0800

    Tweak quoting in getday.sh.

--- 2022/python/getday.sh
+++ 2022/python/getday.sh
@@ -11,6 +11,6 @@ DAYNUM=${(pl:2::0:)ARGUMENT}; # Fun with zsh variable expansion rules.
 tmux split-window -h -c "#{pane_current_path}";
 tmux select-pane -t .0;
 python fetch.py "${DAYNUM}";
-tmux send-keys -t .1 "cat data/input-""${DAYNUM}"".txt";
+tmux send-keys -t .1 "cat data/input-${DAYNUM}.txt";
 tmux send-keys -t .1 "python day.py ${DAYNUM}";
-nvim -o2 "data/puzzle-""${DAYNUM}"".md" "days/day-""${DAYNUM}"".py" tutil.py
+nvim -o2 "data/puzzle-${DAYNUM}.md" "days/day-${DAYNUM}.py" "tutil.py"
