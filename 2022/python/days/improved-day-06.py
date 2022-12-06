320ddf82231c27aa6a3d94c8321fce00af6b9a3b
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2022-12-05 23:42:15 -0800

    2022 day #06 improved refactor.

--- 2022/python/days/improved-day-06.py
+++ 2022/python/days/improved-day-06.py
@@ -4,19 +4,20 @@ TEST_ANSWERS = (7, 19)
 PUZZLE_ANSWERS = (1582, 3588)
 
 
-def find_first_unique(sequences):
-    return "".join(next(s for s in sequences if len(set(s)) == len(s)))
+def find_end_of_start(data, size):
+    def find_first_heterogenous(sequences):
+        return "".join(next(s for s in sequences if len(set(s)) == size))
+
+    start = find_first_heterogenous(sliding_window(size, data))
+    return size + data.index(start)
 
 
-def preprocess(data):
-    return data.strip()
+preprocess = str.strip
 
 
 def part_one(data):
-    windows = sliding_window(4, data)
-    return 4 + data.index(find_first_unique(windows))
+    return find_end_of_start(data, 4)
 
 
 def part_two(data):
-    windows = sliding_window(14, data)
-    return 14 + data.index(find_first_unique(windows))
+    return find_end_of_start(data, 14)
