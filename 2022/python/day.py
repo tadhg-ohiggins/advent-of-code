a8af3cca5ef916ffeb3fa0be8b5f5ad962bc2050
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2022-12-06 16:19:18 -0800

    Runner framework: move imports towards being able to work from a common shared directory.

--- 2022/python/day.py
+++ 2022/python/day.py
@@ -1,10 +1,40 @@
 import argparse
+from pathlib import Path
 import importlib
+import importlib.util
 import richxerox
 from tutils import (
     load_and_process_input,
     run_tests,
 )
+
+"""
+python day.py [day] [optional: "o"(default)/"i"/"b"]
+
+Looks in the days/ subdirectory for day-[day].py and imports from it the
+functions:
+
++   preprocess
++   part_one
++   part_two
+
+It also imports these tuples containing test and puzzle answers:
+
++   TEST_ANSWERS
++   PUZZLE_ANSWERS
+
+It looks in the data/ subdirectory for input-[day].txt and test-input-[day].txt
+and handles loading their contents, running test and puzzle answers, and
+printing answers; it also copies the latest answer to the MacOS clipboard.
+
+The default for the optional script arg does the above. Values for this can
+start with:
+
++   "o" for the original first-pass script (the default).
++   "i" for an improved version, which will cause the script to look in days/
+    for improved-day-[day].py.
++   "b" for both.
+"""
 
 
 def setup_cli_parser() -> argparse.ArgumentParser:
@@ -14,10 +44,26 @@ def setup_cli_parser() -> argparse.ArgumentParser:
     return parser
 
 
+def load_module_from_path(name, filepath, improved=False):
+    spec = importlib.util.spec_from_file_location(name, filepath)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+
 def run_day(daynum, improved=False):
     imprstr = "improved-" if improved else ""
-    module = f"{imprstr}day-{daynum}"
-    day = importlib.import_module(f".{module}", "days")
+    modulename = f"{imprstr}day-{daynum}"
+    modulepath = Path(
+        f"/Users/tadhg/vcs/advent-of-code/2022/python/days/{modulename}.py"
+    )
+    # spec = importlib.util.spec_from_file_location(
+    #     "day",
+    #     f"/Users/tadhg/vcs/advent-of-code/2022/python/days/{modulename}.py",
+    # )
+    # day = importlib.util.module_from_spec(spec)
+    # spec.loader.exec_module(day)
+    day = load_module_from_path("day", modulepath)
     puzzle, test = f"data/input-{daynum}.txt", f"data/test-input-{daynum}.txt"
     data = load_and_process_input(puzzle, [day.preprocess])
     run_tests(
