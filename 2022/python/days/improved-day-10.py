950fe7690e89728d1572641a7a116e36da4866af
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2022-12-10 12:54:07 -0800

    2022 day #10 improved refactor.

--- 2022/python/days/improved-day-10.py
+++ 2022/python/days/improved-day-10.py
@@ -29,9 +29,7 @@ PUZZLE_ANSWERS = (17940, "\n".join(two_answer))
 
 
 def parse_line(line):
-    if line.strip() == "noop":
-        return [0]
-    return [0, int(line.split(" ")[1].strip())]
+    return [0] if line == "noop" else [0, int(line.split(" ")[1])]
 
 
 def preprocess(data):
@@ -44,41 +42,42 @@ def part_one(lines):
     cycle, x = 0, 1
     values, stack = [], []
 
-    while data or stack:
+    # Unhappy that I couldn’t do ``while data or stack:`` here instead, but
+    # even though that works—because it doesn’t alter what goes into `values`—I
+    # still don’t like the fact that it runs unnecessary iterations.
+    while cycle < 240:
+
         cycle = cycle + 1
-
-        exc = stack.pop(0) if stack else 0
-        cmd = data.pop(0) if data else []
-
-        stack = stack + cmd
-        x = x + exc
+        x = x + (stack.pop(0) if stack else 0)
+        stack = stack + (data.pop(0) if data else [])
 
         if (cycle - 20) % 40 == 0:
             values = values + [cycle * x]
+        if cycle >= 240:
+            print(cycle)
 
     return sum(values)
 
 
-def part_two(lines):
-    data = lines[:]
-    cycle = 0
-    sprite = [0, 1, 2]
+def part_two(commands):
+    data = commands[:]
+    cycle, x = 0, 1
     lines, stack = [[]], []
 
-    while data or stack:
+    # Unhappy that I couldn’t do ``while data or stack:`` here instead,  but
+    # there’s an off-by-one error in here that writes a trailing dot if I don’t
+    # stop it at cycle == 239.
+    while cycle < 240:
 
-        char = "█" if ((cycle) % 40) in sprite else "·"
+        cycle = cycle + 1
+        x = x + (stack.pop(0) if stack else 0)
+        stack = stack + (data.pop(0) if data else [])
+
+        char = "█" if (cycle - 1) % 40 in (x - 1, x, x + 1) else "·"
         lines[-1].append(char)
 
         if len(lines[-1]) == 40:
             lines.append([])
-
-        stack = stack + (data.pop(0) if data else [])
-
-        exc = stack.pop(0) if stack else 0
-        sprite = [pos + exc for pos in sprite]
-
-        cycle = cycle + 1
 
     # Extra opening "\n" to make the runner harness print it nicely:
     return "\n" + "\n".join("".join(line) for line in lines).strip()
