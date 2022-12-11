60a5d0b446c1fa41f37d6e309e71416a26c274cd
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2022-12-11 14:23:27 -0800

    2022 day #11 improved refactor.

--- 2022/python/days/improved-day-11.py
+++ 2022/python/days/improved-day-11.py
@@ -1,28 +1,18 @@
+from collections import Counter
 from functools import partial, cache
-from itertools import permutations
-from string import ascii_lowercase, ascii_uppercase
 import math
 import re
 
-from more_itertools import chunked
-from toolz import (
-    compose_left,
-    pipe,
-    sliding_window,
-)
+from toolz import pipe, pluck
 
-from tutils import trace, splitblocks
+from tutils import splitblocks
 from tadhg_utils import (
     p_lmap as cmap,  # Partial/curryable version of map that return a list.
     lcompact,
-    lfilter,
     lmap,  # A version of map that returns a list.
     splitstriplines,
     splitstrip,
-    star,
 )
-import pdb
-
 
 TEST_ANSWERS = (10605, 2713310158)
 PUZZLE_ANSWERS = (113220, 30599555965)
@@ -40,8 +30,8 @@ def parse_block(block):
         "items": lmap(int, splitstrip(items_str, sep=",")),
         "operation": operation,
         "test": int(splitstrip(lines[3], sep="divisible by")[1]),
-        "true": int(splitstrip(lines[4], sep="throw to monkey")[1]),
-        "false": int(splitstrip(lines[5], sep="throw to monkey")[1]),
+        True: int(splitstrip(lines[4], sep="throw to monkey")[1]),
+        False: int(splitstrip(lines[5], sep="throw to monkey")[1]),
     }
     return (monkey_number, data)
 
@@ -52,58 +42,14 @@ def monkey_dict(blocks):
 
 @cache
 def do_op(opstr, value):
-    op = opstr.replace("old", str(value))
-    return eval(op)
+    opstr = opstr.replace("old", str(value))
+    op = "+" if "+" in opstr else "*"
+    func = sum if op == "+" else math.prod
+    return func(map(int, splitstrip(opstr, sep=op)))
 
 
-def preprocess(data):
-    procs = [
-        splitblocks,
-        partial(lcompact),
-        cmap(parse_block),
-        monkey_dict,
-    ]
-    result = pipe(data, *procs)
-    return result
-
-
-def part_one(primates):
-    monkeys = {**primates}
-
-    counts = {k: 0 for k in monkeys}
-    turn = 0
-
-    while turn < 20:
-        for mid in sorted(monkeys.keys()):
-            mkey = monkeys[mid]
-            to_delete = []
-            for item in mkey["items"]:
-                counts[mid] = counts[mid] + 1
-
-                worry = item
-                op = mkey["operation"].replace("old", str(worry))
-                worry = eval(op)
-                worry = math.floor(worry / 3)
-
-                if worry % mkey["test"] == 0:
-                    target = mkey["true"]
-                    monkeys[target]["items"].append(worry)
-                    if target != mid:
-                        to_delete.append(item)
-                else:
-                    target = mkey["false"]
-                    monkeys[target]["items"].append(worry)
-                    if target != mid:
-                        to_delete.append(item)
-            for gone in to_delete:
-                mkey["items"].remove(gone)
-        turn = turn + 1
-
-    return math.prod(sorted(counts.values())[-2:])
-
-
-def get_worry(opstr, all_evals, test, tests, worry):
-    return worry % math.prod(tests)
+def div_worry(worry: int) -> int:
+    return math.floor(worry / 3)
 
 
 @cache
@@ -116,29 +62,55 @@ def check(divisor: int, value: int) -> bool:
     return (value % divisor) == 0
 
 
-def part_two(monkeys):
-    counts = {k: 0 for k in monkeys}
-    product = math.prod(monkeys[m]["test"] for m in monkeys)
-    manage_worry = partial(mod_worry, product)
-    turn = 0
+def preprocess(data):
+    procs = [
+        splitblocks,
+        partial(lcompact),
+        cmap(parse_block),
+        monkey_dict,
+    ]
+    return pipe(data, *procs)
 
-    while turn < 10000:
-        for mid in sorted(monkeys.keys()):
-            mkey = monkeys[mid]
-            to_delete = []
-            for item in mkey["items"]:
-                counts[mid] = counts[mid] + 1
-                to_delete.append(item)
 
-                worry = do_op(mkey["operation"], item)
-                worry = manage_worry(worry)
+def part_one(monkeys):
+    counts, turn = Counter({k: 0 for k in monkeys}), 0
 
-                key = "true" if check(mkey["test"], worry) else "false"
-                target = monkeys[mkey[key]]
+    while turn < 20:
+        for monkey_id, monkey in monkeys.items():
+            for item in monkey["items"]:
+                counts[monkey_id] += 1
+
+                worry = do_op(monkey["operation"], item)
+                worry = div_worry(worry)
+
+                key = check(monkey["test"], worry)
+                target = monkeys[monkey[key]]
                 target["items"].append(worry)
 
-            for gone in to_delete:
-                mkey["items"].remove(gone)
+            monkey["items"] = []
+        turn = turn + 1
+
+    return math.prod(sorted(counts.values())[-2:])
+
+
+def part_two(monkeys):
+    counts, turn = Counter({k: 0 for k in monkeys}), 0
+    product = math.prod(pluck("test", monkeys.values()))
+    manage_worry = partial(mod_worry, product)
+
+    while turn < 10000:
+        for monkey_id, monkey in monkeys.items():
+            for item in monkey["items"]:
+                counts[monkey_id] += 1
+
+                worry = do_op(monkey["operation"], item)
+                worry = manage_worry(worry)
+
+                key = check(monkey["test"], worry)
+                target = monkeys[monkey[key]]
+                target["items"].append(worry)
+
+            monkey["items"] = []
         turn = turn + 1
 
     return math.prod(sorted(counts.values())[-2:])
