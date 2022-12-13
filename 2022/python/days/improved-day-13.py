543674b98dd469f8b0af88f71ee29166432fd511
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2022-12-12 22:34:29 -0800

    2022 day #13 improved refactor.

---
+++ 2022/python/days/improved-day-13.py
@@ -0,0 +1,54 @@
+from functools import cmp_to_key
+from itertools import chain
+
+from toolz import pipe
+
+from tutils import splitblocks
+from tadhg_utils import (
+    p_lmap as cmap,  # Partial/curryable version of map that return a list.
+    splitstriplines,
+)
+
+
+TEST_ANSWERS = (13, 140)
+PUZZLE_ANSWERS = (5557, 22425)
+
+
+def eval_block(block):
+    return cmap(eval)(splitstriplines(block))
+
+
+def preprocess(data):
+    procs = [
+        splitblocks,
+        cmap(eval_block),
+    ]
+    return pipe(data, *procs)
+
+
+def sort_pair(item_one, item_two):
+    if isinstance(item_one, int) and isinstance(item_two, int):
+        return item_one - item_two
+
+    if isinstance(item_one, list) and isinstance(item_two, list):
+        for item in zip(item_one, item_two):
+            curr = sort_pair(*item)
+            if curr != 0:
+                return curr
+
+        return len(item_one) - len(item_two)
+
+    if isinstance(item_one, list) and isinstance(item_two, int):
+        return sort_pair(item_one, [item_two])
+    # if isinstance(item_one, int) and isinstance(item_two, list):
+    return sort_pair([item_one], item_two)
+
+
+def part_one(pairs):
+    return sum(i + 1 for i, p in enumerate(pairs) if sort_pair(*p) < 0)
+
+
+def part_two(pairs):
+    singles = list(chain.from_iterable(pairs)) + [[[2]], [[6]]]
+    ordered = sorted(singles, key=cmp_to_key(sort_pair))
+    return (1 + ordered.index([[2]])) * (1 + ordered.index([[6]]))
